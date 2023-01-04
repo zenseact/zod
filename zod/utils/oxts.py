53bb99691fcdd1f763998f26918889441a03d05d
William Ljungbergh <william.ljungbergh@zenseact.com>
2023-01-04 09:43:48 +0100

    Resolve merge conflict

--- zod/utils/oxts.py
+++ zod/utils/oxts.py
@@ -12,15 +12,15 @@ OXTS_TIMESTAMP_OFFSET = datetime(1980, 1, 6, tzinfo=timezone.utc).timestamp()
 
 @dataclass
 class EgoMotion:
-    pose: np.ndarray  # Nx4x4
-    velocity: np.ndarray  # Nx3
-    acceleration: np.ndarray  # Nx3
-    angular_rate: np.ndarray  # Nx3
-    timestamp: np.ndarray  # N - epoch time in seconds (float64)
-    origin_lat_lon: Tuple[float, float]  # (lat, lon) of the origin (which poses are relative to)
+    poses: np.ndarray  # Nx4x4
+    velocities: np.ndarray  # Nx3
+    accelerations: np.ndarray  # Nx3
+    angular_rates: np.ndarray  # Nx3
+    timestamps: np.ndarray  # N - epoch time in seconds (float64)
+    origin_lat_lon: Tuple[float, float]  # (lat, lon) of the origin (which pose are relative to)
 
     def __len__(self) -> int:
-        return len(self.timestamp)
+        return len(self.timestamps)
 
     def get_poses(self, target_ts: Union[np.ndarray, float, np.float64]) -> np.ndarray:
         """Interpolate neighbouring poses to find pose for each target timestamp.
@@ -29,39 +29,72 @@ class EgoMotion:
         Return:
             [N, 4, 4] array of interpolated poses for each timestamp.
         """
-        selfmin, selfmax = np.min(self.timestamp), np.max(self.timestamp)
+        selfmin, selfmax = np.min(self.timestamps), np.max(self.timestamps)
         assert (selfmin <= np.min(target_ts)) and (
             selfmax > np.max(target_ts)
         ), f"targets not between pose timestamps, must be [{selfmin}, {selfmax})"
-        closest_idxs = self.timestamp.searchsorted(
-            target_ts, side="right", sorter=self.timestamp.argsort()
+        # TODO: check if the timestamp exists, then we don't need to interpolate
+        # else interpolate according to below.
+        closest_idxs = self.timestamps.searchsorted(
+            target_ts, side="right", sorter=self.timestamps.argsort()
         )
-        time_diffs = target_ts - self.timestamp[closest_idxs - 1]
-        total_times = self.timestamp[closest_idxs] - self.timestamp[closest_idxs - 1]
+        time_diffs = target_ts - self.timestamps[closest_idxs - 1]
+        total_times = self.timestamps[closest_idxs] - self.timestamps[closest_idxs - 1]
         fractions = time_diffs / total_times
         return interpolate_transforms(
-            self.pose[closest_idxs - 1], self.pose[closest_idxs], fractions
+            self.poses[closest_idxs - 1], self.poses[closest_idxs], fractions
+        )
+
+    def interpolate(self, timestamps: np.ndarray) -> "EgoMotion":
+        """Interpolate ego motion to find ego motion for each target timestamp."""
+        poses = self.get_poses(timestamps)
+        velocities = interpolate_vectors(self.velocities, self.timestamps, timestamps)
+        accelerations = interpolate_vectors(self.accelerations, self.timestamps, timestamps)
+        angular_rates = interpolate_vectors(self.angular_rates, self.timestamps, timestamps)
+
+        return EgoMotion(
+            poses=poses,
+            velocities=velocities,
+            accelerations=accelerations,
+            angular_rates=angular_rates,
+            timestamps=timestamps,
+            origin_lat_lon=self.origin_lat_lon,
         )
 
     @classmethod
-    def from_sequence_oxts(cls, file: h5py.Group) -> "EgoMotion":
-        return cls(
-            pose=file["poses"][()],
-            acceleration=np.stack(
-                [file["accelerationX"], file["accelerationY"], file["accelerationZ"]], axis=1
-            ),
-            # TODO: figure out what order should be here,
-            velocity=np.stack([file["velDown"], file["velForward"], file["velLateral"]], axis=1),
-            angular_rate=np.stack(
-                [file["angularRateX"], file["angularRateY"], file["angularRateZ"]], axis=1
-            ),
-            timestamp=OXTS_TIMESTAMP_OFFSET + file["timestamp"][()] + file["leapSeconds"][()],
-            origin_lat_lon=(file["posLat"][0], file["posLon"][0]),
-        )
+    def from_sequence_oxts(cls, oxts_path: str) -> "EgoMotion":
+        with h5py.File(oxts_path, "r") as file:
+            return cls(
+                poses=file["poses"][()],
+                accelerations=np.stack(
+                    [file["accelerationX"], file["accelerationY"], file["accelerationZ"]], axis=1
+                ),
+                # TODO: figure out what order should be here,
+                velocities=np.stack(
+                    [file["velDown"], file["velForward"], file["velLateral"]], axis=1
+                ),
+                angular_rates=np.stack(
+                    [file["angularRateX"], file["angularRateY"], file["angularRateZ"]], axis=1
+                ),
+                timestamps=OXTS_TIMESTAMP_OFFSET + file["timestamp"][()] + file["leapSeconds"][()],
+                origin_lat_lon=(file["posLat"][0], file["posLon"][0]),
+            )
 
     @classmethod
     def from_frame_oxts(cls, file: h5py.Group) -> "EgoMotion":
-        raise NotImplementedError("William fix hehe")
+        return cls(
+            poses=get_poses_from_oxts(file),
+            accelerations=np.stack(
+                [file["accelerationX"], file["accelerationY"], file["accelerationZ"]], axis=1
+            ),
+            # TODO: figure out what order should be here,
+            velocities=np.stack([file["velDown"], file["velForward"], file["velLateral"]], axis=1),
+            angular_rates=np.stack(
+                [file["angularRateX"], file["angularRateY"], file["angularRateZ"]], axis=1
+            ),
+            timestamps=OXTS_TIMESTAMP_OFFSET + file["timestamp"][()] + file["leapSeconds"][()],
+            origin_lat_lon=(file["posLat"][0], file["posLon"][0]),
+        )
 
     @classmethod
     def from_json(cls, json: dict) -> "EgoMotion":
@@ -101,3 +134,28 @@ def interpolate_transforms(transform_1: np.ndarray, transform_2: np.ndarray, fra
         axis=-2,
     )
     return transform
+
+
+def interpolate_vectors(
+    values: np.ndarray, timestamps: np.ndarray, target_timestamps: np.ndarray
+) -> np.ndarray:
+    """Interpolate vectors to find vector"""
+    assert np.all(
+        timestamps >= np.min(target_timestamps)
+    ), "target timestamps must be within timestamps"
+    assert np.all(
+        timestamps <= np.max(target_timestamps)
+    ), "target timestamps must be within timestamps"
+    return np.interp(target_timestamps, timestamps, values, left=values[0], right=values[-1])
+
+
+def get_poses_from_oxts(file: h5py.Group) -> np.ndarray:
+    """Get poses from oxts file.
+
+    Args:
+        file: oxts file.
+
+    Returns:
+        [N, 4, 4] array of poses.
+    """
+    raise NotImplementedError
