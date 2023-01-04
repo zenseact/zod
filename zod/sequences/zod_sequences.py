f2fff588bc4df2025d15b12725e66f718e7e8b4a
William Ljungbergh <william.ljungbergh@zenseact.com>
2023-01-04 09:37:13 +0100

    Update ZodSequences

--- zod/sequences/zod_sequences.py
+++ zod/sequences/zod_sequences.py
@@ -1,9 +1,24 @@
+import os
+import os.path as osp
+from itertools import repeat
 from pathlib import Path
 from typing import Dict, Union
+import json
+
+from tqdm.contrib.concurrent import process_map
 
 from zod import constants
 from zod.sequences.info import SequenceInformation
 from zod.utils.utils import zfill_id
+
+
+def _create_sequence(sequence_folder: str, dataset_root: str) -> SequenceInformation:
+    with open(osp.join(sequence_folder, "sequence_info.json"), "r") as f:
+        sequence_info = json.load(f)
+
+    sequence_info = SequenceInformation.from_dict(sequence_info)
+    sequence_info.convert_paths_to_absolute(dataset_root)
+    return sequence_info
 
 
 class ZodSequences:
@@ -19,10 +34,27 @@ class ZodSequences:
             **self._val_sequences,
         }
 
-    def _load_sequences():
-        raise NotImplementedError
+    def _load_sequences(self):
+        sequence_folder = osp.join(self._dataset_root, "sequences")
+        folders = [osp.join(sequence_folder, f) for f in os.listdir(sequence_folder)]
+
+        sequences = process_map(
+            _create_sequence,
+            folders,
+            repeat(self._dataset_root),
+            chunksize=1,
+            desc="Loading sequences",
+        )
+
+        # TODO: fix this
+        return {s.sequence_id: s for s in sequences[:900]}, {
+            s.sequence_id: s for s in sequences[900:]
+        }
 
     def __getitem__(self, sequence_id: Union[int, str]) -> SequenceInformation:
         """Get sequence by id, which is zero-padded number."""
         sequence_id = zfill_id(sequence_id)
         return self._sequences[sequence_id]
+
+    def __len__(self) -> int:
+        return len(self._sequences)
