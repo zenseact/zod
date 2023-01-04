d69e6be1d56f1d689041c7c30aea2d4a2b51e964
William Ljungbergh <william.ljungbergh@zenseact.com>
2023-01-04 16:03:55 +0100

    Change filestrucutre

---
+++ zod/zod_sequences.py
@@ -0,0 +1,61 @@
+import json
+import os
+import os.path as osp
+from itertools import repeat
+from pathlib import Path
+from typing import Dict, Union
+
+from tqdm.contrib.concurrent import process_map
+
+from zod import constants
+from zod.sequences.info import ZodSequenceInformation
+from zod.sequences.zod_sequence import ZodSequence
+from zod.utils.utils import zfill_id
+
+
+def _create_sequence(sequence_folder: str, dataset_root: str) -> ZodSequenceInformation:
+    with open(osp.join(sequence_folder, "sequence_info.json"), "r") as f:
+        sequence_info = json.load(f)
+
+    sequence_info = ZodSequenceInformation.from_dict(sequence_info)
+    sequence_info.convert_paths_to_absolute(dataset_root)
+    return sequence_info
+
+
+class ZodSequences:
+    def __init__(self, dataset_root: Union[Path, str], version: str):
+        self._dataset_root = dataset_root
+        self._version = version
+        assert (
+            version in constants.VERSIONS
+        ), f"Unknown version: {version}, must be one of: {constants.VERSIONS}"
+        self._train_sequences, self._val_sequences = self._load_sequences()
+        self._sequences: Dict[str, ZodSequenceInformation] = {
+            **self._train_sequences,
+            **self._val_sequences,
+        }
+
+    def _load_sequences(self):
+        sequence_folder = osp.join(self._dataset_root, "sequences")
+        folders = [osp.join(sequence_folder, f) for f in os.listdir(sequence_folder)]
+
+        sequences = process_map(
+            _create_sequence,
+            folders,
+            repeat(self._dataset_root),
+            chunksize=1,
+            desc="Loading sequences",
+        )
+
+        # TODO: fix this
+        return {s.sequence_id: s for s in sequences[:900]}, {
+            s.sequence_id: s for s in sequences[900:]
+        }
+
+    def __getitem__(self, sequence_id: Union[int, str]) -> ZodSequence:
+        """Get sequence by id, which is zero-padded number."""
+        sequence_id = zfill_id(sequence_id)
+        return ZodSequence(self._sequences[sequence_id])
+
+    def __len__(self) -> int:
+        return len(self._sequences)
