fa6651bbfe3b51a94ddc17b8566c7b1fae9ded72
Adam Tonderski <adam.tonderski@zenseact.com>
2023-03-28 17:43:54 +0200

    :wastebasket: Add backwards compatibility for oxts.py, with deprecation warning

---
+++ zod/data_classes/oxts.py
@@ -0,0 +1,11 @@
+"""Backwards compatibility for oxts.py."""
+import warnings
+
+from .ego_motion import *
+
+warnings.warn(
+    "The 'zod.data_classes.oxts' module has been deprecated and will be removed in a future "
+    "version. Please use the 'zod.data_classes.ego_motion' module instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
